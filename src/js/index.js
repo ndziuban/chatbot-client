107c8fa10316e6f205bba71331cd845cdbb6606a
Nicolas Dziuban <nicolas.dziuban@gmail.com>
2017-05-29 13:01:01 -0300

    .

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
 'use strict';
 // const Company = require('company');
 
-exports.test = function() {
+function test() {
   console.log("First ajax!")
   $.ajax({
     type: "GET",
-    dataType: "jsonp",
+    dataType: "JSON",
     headers: {"Access-Control-Allow-Origin": "*"},
     url: 'http://www.lutionbot.com/api/company/tan%20intensa?company=true&searchUrl=true&startConfig=true&greetingMsg=true',
     success: function(data) {
@@ -19,7 +19,7 @@ exports.test = function() {
   });
 }
 
-expots.test2 = function() {
+function test2() {
   var company = new Company();
   $(".chat-box").append('<br><span class="chat-line">'+company.getText()+'</span>');
 }
